8c2a5a3b069e1b4fc82b6bba389cb4b6a97b0401
alex <windev@nate.com>
2024-03-08 04:04:27 +0900

    초기 커밋.

---
+++ 함수호출함수포인터함수객체.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct FuncObject
+{
+public:
+	// 함수호출연산자 정의
+	void operator() (int arg) const
+	{
+		cout << "정수: " << arg << endl; 
+	}
+};
+
+void Print1(int arg)
+{
+	cout << "정수: " << arg << endl; 
+}
+
+int main()
+{
+	void (*Print2)(int) = Print1; 
+	FuncObject Print3; 
+
+	Print1(10);  // 첫째, '함수'를 사용한 정수 출력 
+	Print2(10);  // 둘째, '함수포인터'를 사용한 정수 출력 
+	Print3(10);  // 셋째, '함수객체'를 사용한 정수 출력 (Print3.operator(10)과 같음 
+
+	return 0;
+}
